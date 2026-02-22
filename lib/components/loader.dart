edff15f0725081a29ab99d3b3a2bba6535355bd6
akiva <akiva.bekenstein@proton.me>
2026-02-22 10:56:30 +0200

    started building the backend

--- lib/components/loader.dart
+++ lib/components/loader.dart
@@ -1,10 +1,4 @@
-import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_eval/flutter_eval.dart';
-import 'package:orchastrator_sdk/orchastrator_sdk.dart';
 
 class ObjectiveLoader extends StatelessWidget {
   final Uri path;
@@ -16,44 +10,8 @@ class ObjectiveLoader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    StreamController<Signal> state = StreamController();
-    final stateFile = File("$path${Platform.pathSeparator}savedState.txt");
-    stateFile.readAsLines().then((lines) {
-      for (var line in lines) {
-        state.add(Signal.fromJson(jsonDecode(line)));
-      }
-      state.stream.listen((signal) {
-        stateFile.writeAsStringSync(jsonEncode(signal) + Platform.lineTerminator, mode: FileMode.append);
-      });
-    });
-    Widget objective;
-    objective = RuntimeWidget(
-      uri: Uri.file("file://$path${Platform.pathSeparator}objective.evc"),
-      library: "package:objective/objective.dart",
-      function: "Objective",
-      args: [state],
-      onError: _errorWidgetBuilder,
-      loading: CircularProgressIndicator(
-        semanticsLabel: "Loading objective...",
-      ),
-    );
-    if (objective is! ObjectiveWidget) {
-      objective = _errorWidgetBuilder(
-          context,
-          FormatException("Widget doesn't implement the Channel interface"),
-          null);
-    }
     return Card(
-      child: objective,
+      child: Placeholder()
     );
   }
 }
-
-Widget _errorWidgetBuilder(
-        BuildContext context, Object error, StackTrace? stackTrace) =>
-    Container(
-        color: Colors.red,
-        child: Column(children: [
-          Text(error.toString()),
-          Text(stackTrace?.toString() ?? "No stackTrace provided")
-        ]));
